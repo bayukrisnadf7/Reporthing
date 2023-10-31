ae0d7d5aa38f503d402a3e9cd2f4e7a6ea568089
maulana2345 <naufalimal171@gmail.com>
2023-10-31 08:24:43 +0700

    Nilai sumatif

---
+++ php/nilai_sumatif/indexsumatif.php
@@ -0,0 +1,192 @@
+<?php
+include '../../koneksi.php';
+session_start();
+
+$query = "SELECT * FROM tb_mapel";
+$sql = mysqli_query($conn, $query);
+$no = 0;
+?>
+
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="light">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>GACOR KANG</title>
+    <!-- Bootstrap -->
+    <link href="../../asset/css/bootstrap.min.css" rel="stylesheet">
+    <script src="../../asset/js/bootstrap.bundle.min.js"></script>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../../asset/fontawesome/css/all.min.css">
+    <title>Reporthing</title>
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" /> -->
+    <!-- <script src="https://kit.fontawesome.com/ae360af17e.js" crossorigin="anonymous"></script> -->
+    <link rel="stylesheet" href="../../asset/css/style.css" />
+</head>
+
+<body>
+    <!-- ======== Main wrapper for dashboard =========== -->
+
+    <div class="wrapper">
+        <!-- =========== Sidebar for admin dashboard =========== -->
+
+        <aside id="sidebar">
+            <!-- ======== Content For Sidebar ========-->
+            <div class="h-100">
+                <div class="sidebar-logo">
+                    <a href="#"><img src="../../img/logo_biru_muda2.png" alt="homepage" class="dark-logo"
+                        style="width: 10%; margin-right: 2px; margin-bottom: 3px;" /> Reporthing</a>
+                </div>
+                <ul class="sidebar-nav">
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <i class="fa-solid fa-gauge pe-2"></i>
+                            Dashboard
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link active">
+                            <i class="fa-solid fa-chalkboard pe-2"></i>
+                            Nilai Sumatif
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <i class="fa-solid fa-book pe-2"></i>
+                            Nilai Sikap
+                        </a>
+                    </li>
+                </ul>
+                <!-- ======= Navigation links for sidebar ======== -->
+                <ul class="sidebar-nav"></ul>
+            </div>
+        </aside>
+        <!-- ========= Main section of dashboard ======= -->
+        <div class="main">
+            <!-- ========= Main navbar section of dashboard ======= -->
+            <nav class="navbar navbar-expand px-3 border-bottom">
+                <button class="btn" id="sidebar-toggle" type="button">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="navbar-collapse navbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown-toggle d-flex align-items-center">
+                            <a href="#" data-bs-toggle="dropdown" class="nav-icon pe-md-0">
+                                <img src="../../img/seiya.png" class="avatar img-fluid rounded-circle" alt="" />
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end">
+                                <a href="#" class="dropdown-item">Profile</a>
+                                <a href="#" class="dropdown-item">Setting</a>
+                                <a href="#" class="dropdown-item">Logout</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <!-- ========= Main content section of dashboard ======= -->
+            <main class="content px-3 py-2">
+                <div class="content-fluid">
+                    <div class="mb-3">
+                        <h4>Daftar Mata Pelajaran</h4>
+                        <h6>Halaman untuk mengelola nilai sumatif</h6>
+                    </div>
+
+                    <?php
+                    if (isset($_SESSION['eksekusi'])):
+                        ?>
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            <?php
+                            echo $_SESSION['eksekusi'];
+                            ?>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                        <?php
+                        session_destroy();
+                    endif;
+                    ?>
+                    
+                    <!-- Table Element -->
+                    <div class="card border-0">
+                        <div class="card-header" style="background-color: #FFFFFF;">
+                            <!-- <a href="kelolamapel.php" type="button" class="btn btn-primary btn-sm">
+                                <i class="fas fa-plus"></i> Tambah Data
+                            </a> -->
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-hover">
+                                    <thead class="custom-header">
+                                        <tr>
+                                            <th scope="col">No.</th>
+                                            <th scope="col">Id Mapel</th>
+                                            <th scope="col">Nama Mapel</th>
+                                            <th scope="col">Aksi</th>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        while ($result = mysqli_fetch_assoc($sql)) {
+                                            ?>
+                                            <td>
+                                                <?php echo ++$no; ?>.
+                                            </td>
+                                            <td>
+                                                <?php echo $result['id_mapel']; ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $result['nama_mapel']; ?>
+                                            </td>
+                                            <!-- Button UBAH dan HAPUS-->
+                                            <td>
+                                                <a href="#?ubah=<?php echo $result['id_mapel']; ?>"
+                                                    type="button" class="btn btn-warning btn-sm">
+                                                    <i class="fa fa-pen"></i>
+                                                </a>
+                                                <!-- <a href="prosesmapel.php?hapus=<?php echo $result['id_mapel']; ?>"
+                                                    type="button" class="btn btn-danger btn-sm"
+                                                    onClick="return confirm('Ingin menghapus data tersebut?')">
+                                                    <i class="fa fa-trash"></i>
+                                                </a> -->
+                                            </td>
+                                            </tr>
+                                            <?php
+                                        }
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+
+            <!-- ========= light and dark mode toggle button ======= -->
+
+            <!-- <a href="#" class="theme-toggle">
+                <i class="fa-regular fa-moon"></i>
+                <i class="fa-regular fa-sun"></i>
+            </a> -->
+
+            <!-- ========= footer section of dashboard ======= -->
+
+            <!-- <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row text-muted">
+                        <div class="col-6 text-start">
+                            <p class="mb-0">
+                                <a href="#" class="text-muted">
+                                    <strong>Reporthing</strong>
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </footer> -->
+        </div>
+    </div>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"></script> -->
+    <script src="../../asset/js/script.js"></script>
+</body>
+
+</html>
