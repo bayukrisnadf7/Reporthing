50575b81b0f62c04f12f1a93ad2a3c390dd418a0
mochalfannn <mochalfannn@gmail.com>
2023-11-27 08:54:03 +0700

    menambahkan landingpage

---
+++ landingpage/LandingPage.php
@@ -0,0 +1,351 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../asset/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="asset/css/owl.carousel.min.css" />
+    <link rel="stylesheet" href="../asset/fontawesome/css/all.min.css" />
+    <link rel="stylesheet" href="style.css" />
+    <title></title>
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div class="row">
+          <div class="col-md">
+            <div class="brand">
+              <img src="83790f2b43f00be.png" alt="" />
+              <div class="brand-name">
+                <h1>SDN jumerto 02</h1>
+                <h3>Lorem, ipsum dolor.</h3>
+              </div>
+            </div>
+          </div>
+          <!-- <div class="col-md-4 pembungkus-searchbox">
+              <div class="searchbox">
+                <form method="get">
+                  <div class="input-group">
+                    <input class="form-control" type="text" name=""
+                    placeholder="cari sesuatu..." aria-label="cari"
+                    aria-describedby="tombol-cari">
+                  <div class="input-group-append">
+                    <button class="btn btn-primary" id="tombol-cari">text</button>
+                  </div>
+                  </div>
+                </form>
+              </div>
+          </div> -->
+        </div>
+        <!-- row -->
+      </div>
+      <!-- container -->
+    </header>
+    <!-- section menu -->
+
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-nav">
+      <div class="container">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#my-nav" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="my-nav">
+          <ul class="navbar-nav mx-auto mb-2 mb-lg-1">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#">Beranda</a>
+            </li>
+
+            <div class="nav-item dropdown">
+              <a href="#" class="nav-link dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false"> Profile </a>
+              <div class="dropdown-menu" aria-labelledby="my-dropdown">
+                <a class="dropdown-item" href="#profile">Profile</a>
+                <a class="dropdown-item" href="#misi">Visi</a>
+                <a class="dropdown-item" href="#misi">Misi</a>
+              </div>
+            </div>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#prestasi">Prestasi</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#galeri">Galeri</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#ekskul">Ekskul</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#alamat">alamat</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav m-2 m-lg-1">
+            <li class="nav-item-btn">
+              <button type="button" class="btn"><a href="login/indexlogin.php">login</a></button>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!-- container -->
+    </nav>
+    <!-- akhir navbar -->
+
+    <!-- section hero area -->
+    <!-- <section id="gambar-carousel"> -->
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="6.jpg" class="d-block w-100" alt="..." />
+          <div class="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="5.png" class="d-block w-100" alt="..." />
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Second slide label</h5>
+            <p>Some representative placeholder content for the second slide.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="5.png" class="d-block w-100" alt="..." />
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Third slide label</h5>
+            <p>Some representative placeholder content for the third slide.</p>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+    <!-- </section> -->
+    <!-- akhir carousel -->
+
+    <!-- section profile -->
+    <section id="#profile" class="profile">
+      <div class="container">
+        <div class="row">
+          <div class="col-md mb-4 mt-4 Judul-page text-center">
+            <h1 id="profile">Profile</h1>
+          </div>
+        </div>
+        <div class="row sub-judul">
+          <div class="col-md-4">
+            <h1 id="profile">Profile</h1>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, ipsum? Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quos corporis laudantium! Lorem ipsum dolor
+              sit amet consectetur adipisicing elit. Nostrum exercitationem aperiam eaque?
+            </p>
+          </div>
+          <div class="col-md-4">
+            <h1 id="visi">Visi</h1>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi ex inventore laborum veniam porro reiciendis. Sapiente iste rem maxime sequi! Commodi, ipsum?Lorem
+              ipsum dolor sit, amet consectetur adipisicing elit. Quos, nihil laboriosam!
+            </p>
+          </div>
+          <div class="col-md-4">
+            <h1 id="misi">Misi</h1>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, ipsum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quam rem inventore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+              repudiandae at error officiis expedita voluptate cupiditate tenetur dolorum maiores quisquam!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- akhir section -->
+
+    <!-- section Prestasi card-->
+    <section id="prestasi" class="prestasi">
+      <div class="container">
+        <div class="row">
+          <div class="col-md mb-4 mt-4 Judul-page-2 text-center">
+            <h1 id="prestasi">Prestasi</h1>
+          </div>
+        </div>
+        <div class="row sub-judul-2">
+          <div class="col-md-4">
+            <div class="card shadow">
+              <img src="trophy.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card shadow">
+              <img src="trophy.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card shadow">
+              <img src="trophy.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- akhir section -->
+
+    <section id="galeri" class="galeri">
+      <div class="container">
+        <div class="row">
+          <div class="col-md mb-4 mt-4 Judul-page-3 text-center">
+            <h1 id="prestasi">Galeri</h1>
+          </div>
+        </div>
+        <div class="row justify-content-center sub-judul-3">
+          <div class="col">
+            <div class="card">
+              <img src="trophy.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img src="trophy.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img src="trophy.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- akhir section -->
+
+    <section id="ekskul" class="ekskul">
+      <div class="container">
+        <div class="row">
+          <div class="col-md mb-4 mt-4 Judul-page-3 text-center">
+            <h1 id="prestasi">Ekskul</h1>
+          </div>
+        </div>
+        <div class="row justify-content-center sub-judul-3">
+          <div class="col">
+            <div class="card">
+              <img src="trophy.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img src="trophy.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img src="trophy.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- akhir section -->
+
+    <section id="alamat" class="maps">
+      <div class="container maps">
+        <div class="row">
+          <div class="col maps">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3949.659688305456!2d113.6904432225406!3d-8.136084942871358!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd69538e517bb3d%3A0x4f3fa5ebad39a24f!2sSDN%20Jumerto%2002!5e0!3m2!1sen!2sid!4v1700703485281!5m2!1sen!2sid"
+              style="border: 0"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- akhir section -->
+
+    <section class="bawah">
+      <div class="container footer mt">
+        <div class="row footer">
+          <div class="col-md-4">
+            <!-- <div class="card shadow"> -->
+            <!-- <img src="trophy.jpg" class="card-img-top" alt="..." /> -->
+            <!-- <div class="card-body"> -->
+            <h1 class="text-center mt-3 fs-4">alamat</h1>
+            <p class="card-text-footer">
+              Some quick example text to build on the card title and make up the bulk of the card's content Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam aut officiis provident, quisquam soluta vitae animi facilis harum
+              reiciendis praesentium?
+            </p>
+            <!-- </div> -->
+            <!-- </div> -->
+          </div>
+          <div class="col-md-4">
+            <!-- <div class="card shadow"> -->
+            <!-- <img src="trophy.jpg" class="card-img-top" alt="..." /> -->
+            <!-- <div class="card-body"> -->
+            <h1 class="text-center mt-3 fs-4">tentang</h1>
+            <p class="card-text-footer">
+              Some quick example text to build on the card title and make up the bulk of the card's content Lorem ipsum dolor Lorem ipsum dolor, sit amet consectetur adipisicing elit. In eius sunt iste.sit amet consectetur adipisicing
+              elit.Iste incidunt voluptatem odio?
+            </p>
+            <!-- </div> -->
+            <!-- </div> -->
+          </div>
+          <div class="col-md-4">
+            <!-- <div class="card shadow"> -->
+            <!-- <img src="trophy.jpg" class="card-img-top" alt="..." /> -->
+            <!-- <div class="card-body"> -->
+            <h1 class="text-center mt-3 fs-4">sosial media</h1>
+            <p class="card-text-footer">
+              Some quick example text to build on the card title and make up the bulk of the card's content. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, accusamus nesciunt! Animi rem iure perspiciatis.
+            </p>
+            <!-- </div> -->
+            <!-- </div> -->
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- akhir section -->
+
+    <footer>
+      <p class="text-center"><i class="fa-regular fa-copyright"></i> SDN Jumerto 02</p>
+    </footer>
+    <script src="../asset/js/bootstrap.bundle.min.js"></script>
+    <!-- <script src="asset/js/owl.carousel.min.js"></script> -->
+    <script src="../asset/datatables/jQuery-3.7.0/jquery-3.7.0.min.js"></script>
+  </body>
+</html>
