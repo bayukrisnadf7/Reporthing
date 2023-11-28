3ef14cb36b7aadb80ab8968a573416d89b5639c2
maulana2345 <naufalimal171@gmail.com>
2023-11-28 09:39:24 +0700

    Fix fungsi ipas

--- php/kelas1_ganjil_2023/IPAS/fungsi_ipas.php
+++ php/kelas1_ganjil_2023/IPAS/fungsi_ipas.php
@@ -11,26 +11,29 @@ function tambah_data($data)
     $id_tahunajaran = $data['id_tahunajaran'];
     $id_kelas = $data['id_kelas'];
 
-    // Cek apakah NISN sudah ada
-    $queryCekNISN = "SELECT * FROM tb_nilai WHERE nisn = '$nisn'";
-    $resultCekNISN = mysqli_query($GLOBALS['conn'], $queryCekNISN);
 
-    if (mysqli_num_rows($resultCekNISN) > 0) {
+    $sqlselect = mysqli_query($GLOBALS['conn'], "SELECT * FROM tb_nilai WHERE nisn = '$nisn' AND id_tahunajaran = '$id_tahunajaran'  AND id_mapel = '$id_mapel' AND id_kelas = '$id_kelas'");
+
+
+    if (mysqli_num_rows($sqlselect) === 0) {
+
+        $query = "INSERT INTO tb_nilai VALUES('$sumatif', '$sumatif_akhir', '$nilai_rapor','$nisn','$id_mapel', '$id_tahunajaran', '$id_kelas')";
+        $sql = mysqli_query($GLOBALS['conn'], $query);
+
+        $queryShow = "SELECT * FROM tb_total_nilai WHERE nisn = '$nisn' AND id_tahunajaran = 14;";
+        $sqlShow = mysqli_query($GLOBALS['conn'], $queryShow);
+        $result = mysqli_fetch_assoc($sqlShow);
+
+        $query = "UPDATE tb_total_nilai SET ipas = '$nilai_rapor' WHERE nisn='$nisn' AND id_tahunajaran = 14 AND id_kelas = 6;";
+        $sql = mysqli_query($GLOBALS['conn'], $query);
+
+
+    } elseif (mysqli_num_rows($sqlselect) > 0) {
         $_SESSION['eksekusi'] = "Gagal menambahkan data. Nama sudah terdaftar.";
-        header("location: indexsumatif_ipas.php");
+        header("location: indexsumatif_bi.php");
         return false;
+
     }
-
-    $query = "INSERT INTO tb_nilai VALUES('$sumatif', '$sumatif_akhir', '$nilai_rapor','$nisn','$id_mapel', '$id_tahunajaran', '$id_kelas')";
-    $sql = mysqli_query($GLOBALS['conn'], $query);
-
-    $queryShow = "SELECT * FROM tb_total_nilai WHERE nisn = '$nisn' AND id_tahunajaran = 14;";
-    $sqlShow = mysqli_query($GLOBALS['conn'], $queryShow);
-    $result = mysqli_fetch_assoc($sqlShow);
-
-    $query = "UPDATE tb_total_nilai SET ipas = '$nilai_rapor' WHERE nisn='$nisn' AND id_tahunajaran = 14 AND id_kelas = 6";
-    $sql = mysqli_query($GLOBALS['conn'], $query);
-
     return true;
 }
 
@@ -41,18 +44,18 @@ function ubah_data($data)
     $sumatif_akhir = $data['sumatif_akhir'];
     $nilai_rapor = $data['nilai_rapor'];
 
-    $queryShow = "SELECT * FROM tb_nilai WHERE nisn = '$nisn' AND id_mapel = 1 AND id_tahunajaran = 14 AND id_kelas = 6";
+    $queryShow = "SELECT * FROM tb_nilai WHERE nisn = '$nisn' AND id_mapel = 1 AND id_tahunajaran = 14 AND id_kelas = 6;";
     $sqlShow = mysqli_query($GLOBALS['conn'], $queryShow);
     $result = mysqli_fetch_assoc($sqlShow);
 
-    $query = "UPDATE tb_nilai SET sumatif='$sumatif', sumatif_akhir='$sumatif_akhir', nilai_rapor='$nilai_rapor' WHERE nisn='$nisn' AND id_mapel = 1 AND id_tahunajaran = 14 AND id_kelas = 6";
+    $query = "UPDATE tb_nilai SET sumatif='$sumatif', sumatif_akhir='$sumatif_akhir', nilai_rapor='$nilai_rapor' WHERE nisn='$nisn' AND id_mapel = 1 AND id_tahunajaran = 14 AND id_kelas = 6;";
     $sql = mysqli_query($GLOBALS['conn'], $query);
 
-    $queryShow = "SELECT * FROM tb_total_nilai WHERE nisn = '$nisn' AND id_tahunajaran = 14";
+    $queryShow = "SELECT * FROM tb_total_nilai WHERE nisn = '$nisn' AND id_tahunajaran = 14;";
     $sqlShow = mysqli_query($GLOBALS['conn'], $queryShow);
     $result = mysqli_fetch_assoc($sqlShow);
 
-    $query = "UPDATE tb_total_nilai SET ipas = '$nilai_rapor' WHERE nisn='$nisn' AND id_tahunajaran = 14 AND id_kelas = 6";
+    $query = "UPDATE tb_total_nilai SET ipas = '$nilai_rapor' WHERE nisn='$nisn' AND id_tahunajaran = 14 AND id_kelas = 6;";
     $sql = mysqli_query($GLOBALS['conn'], $query);
 
     return true;
